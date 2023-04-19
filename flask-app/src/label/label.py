ce8ad5e11feaf8dd1023a203672e35744f1d18c8
Rohil-doshi <rohil@Rohils-MacBook-Pro.local>
2023-04-19 03:27:55 -0400

    more tests

--- flask-app/src/label/label.py
+++ flask-app/src/label/label.py
@@ -16,26 +16,6 @@ def get_artists():
         json_data.append(dict(zip(row_headers, row)))
     return jsonify(json_data)
 
-@label.route('/getArtistDetails', methods=['GET'])
-def get_artist_by_artistID():
-    cursor = db.get_db().cursor()
-    artistID = request.args.get('artist_id')
-    query = '''
-select * from Artist 
-         where ArtistID =
-'''
-    query += str(artistID)
-
-    cursor.execute(query)
-    column_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(column_headers, row)))
-
-    return jsonify(json_data)
-
-
 # Get all tours in a Country and State
 @label.route('/getRegionTours', methods=['GET'])
 def get_region_tours():
@@ -55,6 +35,7 @@ def get_region_tours():
     query += state
     query += "        order by DateAndTime"
 
+
     cursor.execute(query)
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -69,7 +50,7 @@ def add_merch():
     data = request.get_json()
     cursor = db.get_db().cursor()
     cursor.execute('INSERT INTO Merchandise (MerchandiseID, MerchandiseName, MerchandiseType, Price) \
-                    VALUES (%s, %s, %s, %s, %s)', 
+                    VALUES (%s, %s, %s, %s)', 
                     (data['MerchandiseID'], data['MerchandiseName'], data['MerchandiseType'], data['Price']))
     db.get_db().commit()
     return 'Success'
@@ -87,16 +68,15 @@ def update_artist():
         UPDATE Artist
         SET LabelID = (Select LabelID
                         From Label
-                        Where LabelName = 
+                        Where LabelName =
     '''
-    query += label_name + ')'
+    query += str(label_name) + ')'
     query += " WHERE StageName = "
-    query += artist_name
+    query += str(artist_name)
     cursor.execute(query)
     db.get_db().commit()
     return 'Success'
 
-# get top 10 most popular artists
 @label.route('/getPopularArtists', methods=['GET'])
 def get_most_pop_artist():
     cursor = db.get_db().cursor()
@@ -115,7 +95,6 @@ def get_most_pop_artist():
 
     return jsonify(json_data)
 
-# get all artists that work with the given producer
 @label.route('/getProducerArtists', methods=['GET'])
 def get_producer_artists():
     cursor = db.get_db().cursor()
@@ -137,24 +116,3 @@ def get_producer_artists():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
-
-# get all merchandise from the database
-@label.route('/getAllMerchandise', methods=['GET'])
-def get_artists():
-    cursor = db.get_db().cursor()
-    cursor.execute('SELECT * FROM Merchandise')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    return jsonify(json_data)
-
-# delete merchandise from the db
-@label.route('/deleteMerch', methods=['DELETE'])
-def delete_artist():
-    cursor = db.get_db().cursor()
-    merchandise_id = request.args.get('merchandise_id')
-    cursor.execute('DELETE FROM Merchandise WHERE MerchandiseID=' + merchandise_id)
-    db.get_db().commit()
-    return 'Success'
